e223e0656c442f7c4e0d042ea7375bc50d6f3d4e
Varun Sharma <sharma.varun9991@gmail.com>
2024-08-09 23:59:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HomeScreen from "./screens/HomeScreen";
+
+function App() {
+  
+
+  return (
+    <>
+      <HomeScreen />
+    </>
+  )
+}
+
+export default App
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AboutUs from '../components/About/AboutUs';
+import Foods from '../components/Foods Showcase/Foods';
+import Banner from '../components/Header/Banner';
+
+const HomeScreen = () => {
+    return (
+        <>
+            <Banner />
+            <Foods />
+            <AboutUs />
+        </>
+    )
+}
+
+export default HomeScreen
