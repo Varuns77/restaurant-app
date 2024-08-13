d2bc0023c40a5d3d24494f87f7011ebc4be5a0ac
Varun Sharma <sharma.varun9991@gmail.com>
2024-08-13 20:31:36 +0530

    Commit

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
 // import AboutUs from '../components/About/AboutUs';
-import Foods from '../components/Foods Showcase/Food';
-import Banner from '../Header/Banner';
+import Foods from "../components/Foods Showcase/Food";
+import Filter from "../components/Filter/Filter";
+import Banner from "../Header/Banner";
 
 const HomeScreen = () => {
-    return (
-        <>
-            <Banner />
-            <Foods />
-            {/* <AboutUs /> */}
-        </>
-    )
-}
+  return (
+    <>
+      <Banner />
+      <div className="flex">
+        <Filter />
+        <Foods />
+      </div>
+      {/* <AboutUs /> */}
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
