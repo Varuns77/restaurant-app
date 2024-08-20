d54483c900a3d8161e94eb6d6fa59959b666a226
Varun Sharma <sharma.varun9991@gmail.com>
2024-08-20 17:57:59 +0530

    Commit

--- src/components/Foods Showcase/Food.jsx
+++ src/components/Foods Showcase/Food.jsx
@@ -4,7 +4,7 @@ import FoodItem from './FoodItem';
 import Skeleton from './Skeleton';
 
 const Foods = () => {
-    const [menuTab, setMenuTab] = useState('Breakfast')
+    const [menuTab, setMenuTab] = useState('veg')
     const [loading, setLoading] = useState(false)
     const [foods] = useFetch();
 
@@ -25,14 +25,13 @@ const Foods = () => {
         <section className="my-12 max-w-screen-xl mx-auto px-6">
             {/* food Menu tab  */}
             <div className="flex items-center justify-center space-x-6">
-                <p className={menuTab === 'Breakfast' ? "active_menu_tab poppins bg-primary" : "menu_tab poppins"} onClick={() => handleMenuTabs('Breakfast')}>Breakfast</p>
-                <p className={menuTab === 'Lunch' ? "active_menu_tab poppins bg-primary" : "menu_tab poppins"} onClick={() => handleMenuTabs('Lunch')}>Lunch</p>
-                <p className={menuTab === 'Dinner' ? "active_menu_tab poppins bg-primary" : "menu_tab poppins"} onClick={() => handleMenuTabs('Dinner')}>Dinner</p>
+                <p className={menuTab === 'veg' ? "active_menu_tab poppins bg-primary" : "menu_tab poppins"} onClick={() => handleMenuTabs('veg')}>Veg</p>
+                <p className={menuTab === 'non-veg' ? "active_menu_tab poppins bg-primary" : "menu_tab poppins"} onClick={() => handleMenuTabs('non-veg')}>Non Veg</p>
             </div>
 
             {/* all foods  */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-12">
-                {foods.filter((item) => menuTab === item.type).map(item => (
+                {foods.filter((item) => menuTab === item.preferencetype).map(item => (
                     loading ? <Skeleton key={item.id} /> : <FoodItem key={item.id} {...item} />
                 ))}
             </div>
