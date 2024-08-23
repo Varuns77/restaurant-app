4ab4c2d290592aca90b45a0b222ec99e700762ce
Varun Sharma <sharma.varun9991@gmail.com>
2024-08-23 23:54:42 +0530

    Commit

--- src/Redux/AddToCart/cartSlice.js
+++ src/Redux/AddToCart/cartSlice.js
@@ -2,16 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // Initial state from local storage, if available
 const storedCart = JSON.parse(localStorage.getItem("cart"));
+const storedTotalAmount = JSON.parse(localStorage.getItem("totalAmount"));
 // const initialState = { items: storedCart || [] };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: storedCart || [],
-    totalAmount: 0,
+    totalAmount: storedTotalAmount || 0,
   },
   reducers: {
-    addOrder: (state, action) => {
+    addItem: (state, action) => {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalAmount += newItem.price;
@@ -28,10 +29,10 @@ const cartSlice = createSlice({
         existingItem.quantity++;
         existingItem.totalPrice += newItem.price;
       }
-      saveCartToLocalStorage(state.items);
+      saveCartToLocalStorage(state.items, state.totalAmount);
       // state.items.push(action.payload);
     },
-    removeOrder: (state, action) => {
+    removeItem: (state, action) => {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalAmount -= existingItem.price;
@@ -42,23 +43,26 @@ const cartSlice = createSlice({
         existingItem.totalPrice -= existingItem.price;
         // state.items = state.items.filter((item) => item.id !== action.payload);
       }
-      saveCartToLocalStorage(state.items);
+      saveCartToLocalStorage(state.items, state.totalAmount);
     },
     clearCart(state) {
       state.items = [];
       state.totalAmount = 0;
+      saveCartToLocalStorage(state.items, state.totalAmount);
       clearCartFromLocalStorage();
     },
   },
 });
 
-const saveCartToLocalStorage = (cart) => {
+const saveCartToLocalStorage = (cart, totalAmount) => {
   localStorage.setItem("cart", JSON.stringify(cart));
+  localStorage.setItem("totalAmount", JSON.stringify(totalAmount));
 };
 
 const clearCartFromLocalStorage = () => {
   localStorage.removeItem("cart");
+  localStorage.removeItem("totalAmount");
 };
 
-export const { addOrder, removeOrder, clearCart } = cartSlice.actions;
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
