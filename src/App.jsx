8c4a6391e6f6f51d0e65bedb8e7ad2080d2b0a8d
Varun Sharma <sharma.varun9991@gmail.com>
2024-08-26 20:39:12 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import AuthProvider from "./contexts/AuthProvider";
+import DeliveryProvider from "./contexts/DeliveryProvider";
+// import OrderProvider from "./contexts/OrderProvider";
+import PrivateRoute from "./routes/PrivateRoute";
+import PublicRoute from "./routes/PublicRoute";
+import ErrorScreen from "./screens/ErrorScreen";
+import FoodDetailScreen from "./screens/FoodDetailScreen";
 import HomeScreen from "./screens/HomeScreen";
+import OrderSuccessfulScreen from "./screens/OrderSuccessfulScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import SignInScreen from "./screens/SignInScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import YourOrders from "./components/PlaceOrder/YourOrders";
 
-function App() {
-  return (
-    <>
-      <HomeScreen />
-    </>
-  )
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <AuthProvider>
+                {/* <OrderProvider> */}
+                    <DeliveryProvider>
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={<HomeScreen />} />
+                            <Route element={<PublicRoute />}>
+                                <Route path="/signin" element={<SignInScreen />} />
+                                <Route path="/signup" element={<SignUpScreen />} />
+                            </Route>
+                            <Route element={<PrivateRoute />}>
+                                <Route path="/foods/:title" element={<FoodDetailScreen />} />
+                                <Route path="/orders" element={<PlaceOrderScreen />} />
+                                <Route path="/orderedItems" element={<YourOrders />} />
+                                <Route path="/order-successful" element={<OrderSuccessfulScreen />} />
+                            </Route>
+                            <Route path="*" element={<ErrorScreen />} />
+                        </Routes>
+                    </DeliveryProvider>
+                {/* </OrderProvider> */}
+            </AuthProvider>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
---
+++ src/routes/PublicRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+const PublicRoute = () => {
+    const { user } = useAuth();
+
+    if (user.displayName) {
+        return <Navigate to="/" replace />;
+    }
+
+    return <Outlet />;
+};
+
+export default PublicRoute;
