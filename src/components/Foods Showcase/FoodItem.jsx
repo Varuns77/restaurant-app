1f39b0378cb3c54dbbd3d182581c9be9d53228f9
Varun Sharma <sharma.varun9991@gmail.com>
2024-09-06 01:02:44 +0530

    Commit

--- src/components/Foods Showcase/FoodItem.jsx
+++ src/components/Foods Showcase/FoodItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom'; // Updated import
 
 const FoodItem = ({ image, title, description, price, type, cuisinetype }) => {
-    const navigate = useNavigate(); // Updated hook from useHistory to useNavigate
+    const navigate = useNavigate(); 
 
     const handleRoute = () => {
         navigate(`/foods/${title}`);
@@ -10,14 +10,14 @@ const FoodItem = ({ image, title, description, price, type, cuisinetype }) => {
 
     return (
         <div className="bg-white border border-gray-100 transition transform duration-700 hover:shadow-xl hover:scale-105 p-4 rounded-lg relative">
-            <span className="bg-blue-100 border border-blue-500 rounded-full text-primary text-sm poppins px-4 py-1 inline-block mb-4 ">{type}</span>
-            <span className="bg-blue-100 border border-blue-500 rounded-full text-primary text-sm poppins px-4 py-1 absolute top-4 right-4 inline-block">{cuisinetype}</span>
+            <span className="bg-blue-100 border border-blue-500 rounded-full text-primary text-sm forum font-bold px-4 py-1 inline-block mb-4 ">{type}</span>
+            <span className="bg-blue-100 border border-blue-500 rounded-full text-primary text-sm forum font-bold px-4 py-1 absolute top-4 right-4 inline-block">{cuisinetype}</span>
             <img className="w-64 mx-auto transform transition duration-300 hover:scale-105" src={image} alt={title} />
             <div className="flex flex-col items-center my-3 space-y-2">
-                <h1 className="text-gray-900 poppins text-lg">{title}</h1>
-                <p className="text-gray-500 poppins text-sm text-center">{description.slice(0, 50)}</p>
-                <h2 className="text-gray-900 poppins text-2xl font-bold">${price}</h2>
-                <button className="bg-primary text-white px-8 py-2 focus:outline-none poppins rounded-full mt-24 transform transition duration-300 hover:scale-105" onClick={handleRoute}>
+                <h1 className="text-gray-900 forum text-2xl font-bold">{title}</h1>
+                {/* <p className="text-gray-500 poppins text-sm text-center">{description.slice(0, 50)}</p> */}
+                <h2 className="text-gray-900 forum text-xl">${price}</h2>
+                <button className="bg-primary text-white px-8 py-2 focus:outline-none forum font-bold rounded-full mt-24 transform transition duration-300 hover:scale-105" onClick={handleRoute}>
                     Order Now
                 </button>
             </div>
--- src/components/Menu/Drinks.jsx
+++ src/components/Menu/Drinks.jsx
@@ -17,6 +17,12 @@ const Drinks = ({ drinkType }) => {
     <>
       {/* Initialize Swiper */}
       <div className="w-full h-full">
+        <div className="flex flex-col items-center justify-center mt-8">
+          <span className="sub-heading font-bold">drinks</span>
+          <span className="main-heading font-bold mb-6">
+            Cheers to Choice: Discover Our Drink Menu
+          </span>
+        </div>
         <Swiper
           modules={[EffectCoverflow, Pagination]}
           effect="coverflow"
@@ -46,19 +52,19 @@ const Drinks = ({ drinkType }) => {
         >
           {Drinks.map((drink) => (
             <SwiperSlide>
-              <div className="border-2 border-solid border-gray-400 bg-white max-w-[480px] lg:max-w-[100%] lg:max-h-[100%] md:max-w-[500px] flex gap-3 items-center justify-start mb-12">
+              <div className="slide-content border-2 border-solid border-gray-400 bg-white max-w-[480px] lg:max-w-[100%] lg:max-h-[100%] md:max-w-[500px] flex gap-3 items-center justify-start mb-12">
                 <div className="h-60 w-44 lg:max-h-[250px] lg:max-w-[180px] md:w-[200px] flex-shrink-0">
                   <img src={drink.image} alt="" className="h-full w-full" />
                 </div>
                 <div className="flex-1">
-                  <h3 className="text-xl text-black pb-1 lg:text-2xl md:text-xl">
+                  <h3 className="text-xl text-black font-bold pb-1 lg:text-2xl md:text-xl">
                     {drink.title}
                   </h3>
-                  <div className="price text-xl text-[#cd9452] mb-1 lg:text-2xl md:text-xl">
+                  <div className="price text-xl mb-1 lg:text-2xl md:text-xl">
                     ${drink.price}
                   </div>
                   <button
-                    className="bg-primary text-white px-2 py-2 text-sm focus:outline-none poppins rounded-full transform transition duration-300 hover:scale-105 block"
+                    className="bg-primary text-white px-3 py-2 text-sm focus:outline-none forum font-bold rounded-full transform transition duration-300 hover:scale-105 block"
                     //   onClick={handleRoute}
                   >
                     Order Now
@@ -67,106 +73,6 @@ const Drinks = ({ drinkType }) => {
               </div>
             </SwiperSlide>
           ))}
-
-          {/* Breakfast Slide */}
-          {/* <SwiperSlide>
-          <div className="text-center p-16 rounded-lg transform scale-90 opacity-50 mb-16 cursor-pointer swiper-slide-active:hover:scale-100 swiper-slide-active:hover:opacity-100 swiper-slide-active:hover:bg-white swiper-slide-active:hover:shadow-md swiper-slide-active:hover:border">
-            <img
-              src="/assets/Beverages/Coke.png"
-              alt=""
-              className="h-64 mb-4 mx-auto"
-            />
-            <h3 className="text-2xl py-4 text-gray-800">delicious food</h3>
-            <div className="text-2xl text-orange-600">$59.99</div>
-          </div>
-        </SwiperSlide> */}
-
-          {/* Lunch Slide */}
-          {/* <SwiperSlide className="swiper-slide slide"> */}
-          {/* <div class="swiper-slide slide" data-name="food-2">
-                <img src="/assets/Beverages/OrangeJuice.png" alt=""/>
-                <h3>delicious food</h3>
-                <div class="price">$59.99</div>
-            </div> */}
-
-          {/* <div className="text-center p-16 rounded-lg transform scale-90 opacity-50 mb-16 cursor-pointer swiper-slide-active:hover:scale-100 swiper-slide-active:hover:opacity-100 swiper-slide-active:hover:bg-white swiper-slide-active:hover:shadow-md swiper-slide-active:hover:border">
-            <img
-              src="/assets/Beverages/OrangeJuice.png"
-              alt=""
-              className="h-64 mb-4 mx-auto"
-            />
-            <h3 className="text-2xl py-4 text-gray-800">delicious food</h3>
-            <div className="text-2xl text-orange-600">$59.99</div>
-          </div>
-        </SwiperSlide> */}
-
-          {/* Dinner Slide */}
-          {/* <SwiperSlide className="swiper-slide slide">
-          <div className="text-center p-16 rounded-lg transform scale-90 opacity-50 mb-16 cursor-pointer swiper-slide-active:hover:scale-100 swiper-slide-active:hover:opacity-100 swiper-slide-active:hover:bg-white swiper-slide-active:hover:shadow-md swiper-slide-active:hover:border">
-            <img
-              src="/assets/Beverages/OrangeJuice.png"
-              alt=""
-              className="h-64 mb-4 mx-auto"
-            />          
-            <h3>delicious food</h3>
-            <div class="price">$59.99</div>
-          </div>
-        </SwiperSlide> */}
-
-          {/* <SwiperSlide className="mb-12">
-          <div className="border-2 border-solid border-gray-400 w-full">
-            <div className="h-72 w-64">
-              <img
-                src="/assets/Beverages/OrangeJuice.png"
-                alt=""
-                className="h-full w-full"
-              />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="border-2 border-solid border-gray-400 w-full">
-            <div className="h-72 w-64">
-              <img
-                src="/assets/Beverages/LemonSoda.png"
-                alt=""
-                className="h-full w-full"
-              />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="border-2 border-solid border-gray-400 bg-white w-full flex gap-3 items-center justify-start">
-            <div className="h-72 w-64">
-              <img
-                src="/assets/Beverages/Coke.png"
-                alt=""
-                className="h-full w-full"
-              />
-            </div>
-            <div className="">
-              <h3 className="text-2xl text-black pb-1">Juice</h3>
-              <div className="price text-2xl text-[#cd9452] mb-1">$8.99</div>
-              <button
-                className="bg-primary text-white px-2 py-1 text-sm focus:outline-none poppins rounded-full transform transition duration-300 hover:scale-105 block"
-                //   onClick={handleRoute}
-              >
-                Order Now
-              </button>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="border-2 border-solid border-gray-400 bg-white w-full">
-            <div className="h-72 w-64 ">
-              <img
-                src="/assets/Beverages/StrawberryJuice.png"
-                alt=""
-                className="h-full w-full"
-              />
-            </div>
-          </div>
-        </SwiperSlide> */}
         </Swiper>
       </div>
     </>
