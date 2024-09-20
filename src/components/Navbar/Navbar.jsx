cf60fb68b523e5d77cb2cc90f9eb7e930e702fce
Varun Sharma <sharma.varun9991@gmail.com>
2024-09-20 15:20:58 +0530

    Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,92 +39,30 @@ const Navbar = () => {
           : "bg-transparent fixed z-50 top-0 left-0 w-full transition duration-500"
       }
     >
-      <nav className="flex items-center max-w-screen-xl mx-auto justify-between">
-        {/* navbar */}
-        <div className="w-screen flex justify-between items-center px-5">
-          <div className="flex backdrop-blur-sm rounded-sm">
-            <img
-              className="w-[5.5rem] cursor-pointer"
-              src={logo}
-              alt="logo"
-              onClick={() => navigate("/")}
-            />
+      <nav className="flex items-center max-w-screen-xl mx-auto justify-between px-5">
+        <div className="flex backdrop-blur-sm rounded-sm">
+          <img
+            className="w-[5.5rem] cursor-pointer"
+            src={logo}
+            alt="logo"
+            onClick={() => navigate("/")}
+          />
+        </div>
+
+        {/* Right Section for Cart and Menu */}
+        <div className="flex items-center space-x-6 md:hidden">
+          {/* Cart Icon */}
+          <div
+            className="relative flex cursor-pointer"
+            onClick={toggleShowCart}
+          >
+            <span className="bg-primary w-6 h-6 rounded-full flex items-center justify-center text-white forum font-bold absolute -right-2 -top-2">
+              {order.length}
+            </span>
+            <BsCart2 className="cursor-pointer w-6 h-6 text-gray-700" />
           </div>
 
-          <div className="flex gap-2 !hidden md:!flex">
-            <p
-              className="forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
-              onClick={() => navigate("/")}
-            >
-              Home
-            </p>
-            <p
-              className="forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
-              onClick={() => navigate("/menu")}
-            >
-              Menu
-            </p>
-            <p
-              className="forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
-              // onClick={() => navigate("/menu")}
-            >
-              Order
-            </p>
-          </div>
-
-          {/* Right  */}
-          {user?.displayName ? (
-            <>
-              <div className="items-center justify-end space-x-4 !hidden md:!flex">
-                <div
-                  className="relative flex cursor-pointer"
-                  onClick={toggleShowCart}
-                >
-                  <span className="bg-primary w-6 h-6 rounded-full flex items-center justify-center text-white forum font-bold absolute -right-2 -top-2">
-                    {order.length}
-                  </span>
-                  <BsCart2 className="cursor-pointer w-6 h-6 text-gray-700" />
-                </div>
-                <img
-                  src={user.photoURL}
-                  alt={user.displayName}
-                  className="w-10 h-10 rounded-full border-black"
-                />
-                <p className="text-gray-700 forum hidden md:block lg:block">
-                  {user.displayName}
-                </p>
-                <FiLogOut
-                  className="cursor-pointer w-6 h-6 text-gray-700"
-                  onClick={signOutUser}
-                />
-              </div>
-              <Cart showCart={showCart} setShowCart={setShowCart} />
-            </>
-          ) : (
-            <>
-              <div className="flex items-center justify-end space-x-6">
-                <button className="forum" onClick={() => navigate("/signin")}>
-                  Sign In
-                </button>
-                <button
-                  className="bg-primary px-6 py-3 text-white forum rounded-full ring-red-300 focus:outline-none focus:ring-4 transform transition duration-700 hover:scale-105"
-                  onClick={() => navigate("/signup")}
-                >
-                  Sign Up
-                </button>
-              </div>
-            </>
-          )}
-
-          {/* Desktop Menu */}
-          {/* <ul className="!hidden md:!flex items-center space-x-5">
-          <li>About</li>
-          <li>Contact</li>
-          <li>Service</li>
-          <li>Help</li>
-        </ul> */}
-
-          {/* Hamburger menu */}
+          {/* Hamburger Menu */}
           <button
             className="space-y-1 md:hidden"
             onClick={() => setMenuOpen(!menuOpen)}
@@ -135,7 +73,65 @@ const Navbar = () => {
           </button>
         </div>
 
-        {/* Menu */}
+        {/* Desktop Menu */}
+        <div className="gap-2 !hidden md:!flex">
+          <p
+            className="forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
+            onClick={() => navigate("/")}
+          >
+            Home
+          </p>
+          <p
+            className="forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
+            onClick={() => navigate("/menu")}
+          >
+            Menu
+          </p>
+          <p className="forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]">
+            Order
+          </p>
+        </div>
+
+        {/* User Section for Desktop */}
+        {user?.displayName ? (
+          <div className="items-center justify-end space-x-4 !hidden md:!flex">
+            <div
+              className="relative flex cursor-pointer"
+              onClick={toggleShowCart}
+            >
+              <span className="bg-primary w-6 h-6 rounded-full flex items-center justify-center text-white forum font-bold absolute -right-2 -top-2">
+                {order.length}
+              </span>
+              <BsCart2 className="cursor-pointer w-6 h-6 text-gray-700" />
+            </div>
+            <img
+              src={user.photoURL}
+              alt={user.displayName}
+              className="w-10 h-10 rounded-full border-black"
+            />
+            <p className="text-gray-700 forum hidden md:block lg:block">
+              {user.displayName}
+            </p>
+            <FiLogOut
+              className="cursor-pointer w-6 h-6 text-gray-700"
+              onClick={signOutUser}
+            />
+          </div>
+        ) : (
+          <div className="flex items-center justify-end space-x-6">
+            <button className="forum" onClick={() => navigate("/signin")}>
+              Sign In
+            </button>
+            <button
+              className="bg-primary px-6 py-3 text-white forum rounded-full ring-red-300 focus:outline-none focus:ring-4 transform transition duration-700 hover:scale-105"
+              onClick={() => navigate("/signup")}
+            >
+              Sign Up
+            </button>
+          </div>
+        )}
+
+        {/* Responsive Dropdown Menu */}
         <ul
           className={`bg-white w-screen pb-5 absolute top-0 right-0 transform ${
             menuOpen ? "translate-y-0" : "-translate-y-full"
@@ -148,21 +144,37 @@ const Navbar = () => {
             <div className="w-6 h-1 bg-black rounded-sm rotate-45 absolute"></div>
             <div className="w-6 h-1 bg-black rounded-sm -rotate-45 absolute"></div>
           </button>
-          <li className="flex justify-center w-full py-4 forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]">
+          <li
+            className="flex justify-center w-full py-4 forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
+            onClick={() => { navigate("/")
+              setMenuOpen(false)}}
+          >
             Home
           </li>
-          <li className="flex justify-center w-full py-4 forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]">
+          <li
+            className="flex justify-center w-full py-4 forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
+            onClick={() => { navigate("/menu")
+              setMenuOpen(false)}
+            }
+          >
             Menu
           </li>
-          <li className="flex justify-center w-full py-4 forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]">
+          <li
+            className="flex justify-center w-full py-4 forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
+            //  onClick={() => navigate("/")}
+          >
             Order
           </li>
-          <li className="flex justify-center w-full py-4 forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]">
+          <li
+            className="flex justify-center w-full py-4 forum text-lg font-bold hover:cursor-pointer hover:text-[#93c5fd]"
+            onClick={signOutUser}
+          >
             Logout
           </li>
         </ul>
       </nav>
     </header>
+
     // <header className={changeHeader ? "bg-white fixed z-50 top-0 left-0 w-full shadow-md transition duration-500" : "bg-transparent fixed z-50 top-0 left-0 w-full transition duration-500"}>
     //     <nav className="flex items-center max-w-screen-xl mx-auto justify-between">
     //         {/* Left  */}
